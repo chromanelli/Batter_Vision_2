6ee20034782471b34533ac543bc9709393366073
chromanelli <cmromanelli09@gmail.com>
2023-07-03 23:59:14 -0500

    save past runs

--- calculations.py
+++ calculations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from config import Config
 
 def make_dicts():
@@ -17,6 +18,25 @@ def make_dicts():
     
     return pitcher_dict, team_dict
 
+def save_data(pitcher_best, batter_dfs):
+    pitcher_id = str(pitcher_best["player_id"].values[0])
+    batting_team = str(batter_dfs[0]["team_name_alt"].values[0])
+    dirName = "data/" + str(Config.end_date) + "_" + pitcher_id + "_" + batting_team
+    
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+    
+    pitcher_file = dirName + "/pitcher_best.csv"
+    pitcher_best.to_csv(pitcher_file)
+
+    for df in batter_dfs:
+        pitchName = str(df["pitch_name"].values[0])
+        fileName = dirName + "/" + batting_team + "_vs_" + pitchName + ".csv"
+        df.to_csv(fileName)
+        print()
+
+    return
+
 def calculate():
     pitcher_dict, team_dict = make_dicts()
     
@@ -25,10 +45,14 @@ def calculate():
     print(pitcher_most_used)
 
     team_df = team_dict[Config.opp_team]
+    batter_dfs = []
     for pitch in pitcher_most_used["pitch_name"].values:
         print(pitch)
         team_pitch_match = team_df[team_df["pitch_name"] == pitch]
         team_highest_ba = team_pitch_match.nlargest(5, ["ba"])
         team_highest_ba = team_highest_ba.sort_values(by="ba", ascending=False)
+        batter_dfs.append(team_highest_ba)
         print(team_highest_ba)
+
+    save_data(pitcher_most_used, batter_dfs)
     return
